747bb6408b00caff966e0eadfd27d2cca2eb8a30
lewei li <lewei.li@qq.com>
2018-04-12 22:34:05 +0800

    add log

--- teb/AppDelegate.m
+++ teb/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //应用启动并进行初始化时会调用该方法。这个阶段会实例化根视图控制器。
+    NSLog(@"%s", "application:didFinishLaunchingWithOptions: Not runing -> Inactive");
     return YES;
 }
 
@@ -24,27 +26,39 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    //应用进入前台非活动状态时调用该方法。这个阶段可以恢复UI的状态。
+
+    NSLog(@"%s","applicationDidBecomeActive: Inactive -> active");
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //应用从活动状态进入到非活动状态时调用该方法。这个阶段可以保存UI的状态。
+    NSLog(@"%s", "applicationWillResignActive: active -> Inactive");
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    //应用进入后台或挂起时调用该方法。这个阶段可以保存用户数据，释放一些资源（例如释放数据库资源等）。
+    NSLog(@"%s","applicationDidEnterBackground: Background -> Suspended");
+
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    //应用从后台再次回到前台，但是还没有处于活动状态时调用该方法。这个阶段可以恢复用户数据。
+    NSLog(@"%s","applicationWillEnterForeground: Suspended -> Background -> Inactive");
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    //应用被终止时调用该方法，但内存清除时除外。 这个阶段释放一些资源，也可以保存用户数据。
+    NSLog(@"%s","applicationWillTerminate: Suspended -> Not runing");  
 }
 
 
--- teb/FirstViewController.m
+++ teb/FirstViewController.m
@@ -17,13 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // 　在视图控制器已被实例化，视图被加载到内存中时，会调用viewDidLoad方法，这时视图并未出现。通常会对所控制的视图进行初始化处理。
+    NSLog(@"%s", "FirstView 视图创建 ：viewDidLoad");
 }
 
-
+- (void)viewWillAppear:(BOOL)animated{
+    NSLog(@"%s", "FirstView 视图可见 ：viewWillAppear");
+    
+}
+-(void)viewDidAppear:(BOOL)animated{
+    NSLog(@"%s", "FirstView 视图可见 ：viewDidAppear");
+    
+}
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    NSLog(@"%s", "FirstView 视图不可见 ：viewDidDisappear");
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    NSLog(@"%s", "FirstView 视图不可见 ：viewWillDisappear");
+    
+}
 - (void)didReceiveMemoryWarning {
+    // 释放内存，包括视图控制器中的一些成员变量和视图的释放。
+    NSLog(@"%s", "FirstView 系统低内存 ：didReceiveMemoryWarning");
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(void)viewDidUnload{
+    NSLog(@"%s", "FirstView 系统低内存 ：viewDidUnload");
+    [super viewDidUnload];
+    
+}
 
 @end
